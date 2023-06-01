b5cef6ae2a8a6972458aff9a71d0567e0dc7eec8
testenet1 <118823851+testenet1@users.noreply.github.com>
2023-06-01 01:14:55 -0300

    Create start_bot.sh

---
+++ start_bot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Verifica se o arquivo de configuração já existe
+if [ -f "/etc/SSHPlus/config.txt" ]; then
+    echo "Arquivo de configuração já existe. Utilizando configurações existentes."
+else
+    # Solicita o token
+    read -p "Digite o token: " token
+
+    # Solicita o ID
+    read -p "Digite o ID: " id
+
+    # Grava as configurações no arquivo
+    echo "TOKEN=$token" > /etc/SSHPlus/config.txt
+    echo "ID=$id" >> /etc/SSHPlus/config.txt
+
+    echo "Configurações salvas com sucesso."
+fi
+
+# Lê as configurações do arquivo
+source "/etc/SSHPlus/config.txt"
+
+# Vai ao diretório desejado
+cd /etc/SSHPlus
+
+# Executa o comando em uma nova tela do screen
+screen -dmS bot_plus bash -c "./bot $TOKEN $ID"

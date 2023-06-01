a99a279de8a173b2ea6164ab3c7fce1d79eac762
testenet1 <118823851+testenet1@users.noreply.github.com>
2023-05-31 23:50:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Baixa script
+# Baixa script
 wget https://raw.githubusercontent.com/testenet1/linux/main/udpvpn.sh
 
 # Conceder permissões adequadas ao script
@@ -8,7 +8,7 @@ chmod +x /usr/local/bin/udpvpn.sh
 
 # Verifica se o arquivo /etc/rc.local está vazio
 if [ ! -s /etc/rc.local ]; then
-    # Adiciona a linha #!/bin/bash ao arquivo
+    # Adiciona a linha #!/bin/bash ao arquivo e adiciona preenchimento do diretório para inicialização
     echo "#!/bin/bash" > /etc/rc.local
     echo "/usr/local/bin/udpvpn.sh" >> /etc/rc.local
 else
@@ -16,5 +16,5 @@ else
     echo "/usr/local/bin/udpvpn.sh" >> /etc/rc.local
 fi
 
-#Remove instalador
+# Remove instalador
 rm /usr/local/bin/install.sh

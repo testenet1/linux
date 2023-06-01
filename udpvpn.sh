c994be7c0d1d7811c710340623a931c0378f4001
testenet1 <118823851+testenet1@users.noreply.github.com>
2023-05-31 22:36:08 -0300

    Update udpvpn.sh

--- udpvpn.sh
+++ udpvpn.sh
@@ -5,9 +5,9 @@ start_port = 7100
 end_port = 7900
 step = 100
 
-current_port=$start_port
+current_port = $start_port
 
 while [ $current_port -le $end_port ]; do
-      screen -dmS udpvpn /bin/udp $current_port
-      current_port=$((current_port + step))
+            screen -dmS udpvpn /bin/udp $current_port
+            current_port=$((current_port + step))
 done

7c6ea7d1b91369b6cd49346af30bcdd5ebb96a0e
testenet1 <118823851+testenet1@users.noreply.github.com>
2023-05-31 22:50:36 -0300

    Update udpvpn.sh

--- udpvpn.sh
+++ udpvpn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Liberando as portas de 7100 até 7900
-start_port = 7100
-end_port = 7900
-step = 100
+start_port=7100
+end_port=7900
+step=100
 
-current_port = $start_port
+current_port=$start_port
 
 while [ $current_port -le $end_port ]; do
         screen -dmS udpvpn /bin/udp $current_port

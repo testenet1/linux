9d1ad622a0cd1989023e7266c1e3f5d59750c4c5
testenet1 <118823851+testenet1@users.noreply.github.com>
2023-06-03 20:50:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#INSTALAÇÃO DO UDPVPN AUTO
+
 # Mover install.sh para diretório
 mv ./install.sh /usr/local/bin
 
@@ -21,5 +23,29 @@ echo "/usr/local/bin/udpvpn.sh" >> /etc/rc.local
 # Inicializar 
 ./udpvpn.sh
 
+#INSTALAÇÃO DO BOT AUTO
+
+# Mover install para diretório
+mv ./install.sh /etc/SSHPlus
+
+# Ir ao diretório desejado
+cd /etc/SSHPlus
+
+# Baixa script
+wget https://raw.githubusercontent.com/testenet1/linux/main/start_bot.sh
+
+# Conceder permissões adequadas ao script
+chmod +x /etc/SSHPlus/start_bot.sh
+
+# Verifica se o arquivo /etc/rc.local está vazio
+if ! grep -q "#!/bin/bash" /etc/rc.local; then
+    echo "#!/bin/bash" > /etc/rc.local
+fi
+echo "/etc/SSHPlus/start_bot.sh" >> /etc/rc.local
+
+#Inicializa o programa.
+
+./start_bot.sh
+
 # Remove instalador
-rm /usr/local/bin/install.sh
+rm /etc/SSHPlus/install.sh

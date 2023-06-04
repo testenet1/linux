08ee3255145400e0dee7df85d47d255154402e39
testenet1 <118823851+testenet1@users.noreply.github.com>
2023-06-04 19:01:09 -0300

    Update install_bot.sh

--- install_bot.sh
+++ install_bot.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Mover install_bot para diretório
-mv ./install_bot.sh /etc/SSHPlus
+mv ./install_bot.sh /usr/local/bin
 
 # Ir ao diretório desejado
-cd /etc/SSHPlus
+cd /usr/local/bin
 
 # Baixa script
 wget https://raw.githubusercontent.com/testenet1/linux/main/botteste
 
 # Conceder permissões adequadas ao script
-chmod +x /etc/SSHPlus/botteste
+chmod +x /usr/local/bin/botteste
 
 # Verifica se o arquivo /etc/rc.local está vazio
 if ! grep -q "#!/bin/bash" /etc/rc.local; then
     echo "#!/bin/bash" > /etc/rc.local
 fi
-echo "cd /etc/SSHPlus" >> /etc/rc.local
+echo "cd /usr/local/bin" >> /etc/rc.local
 echo "screen -dmS bot_teste ./botteste" >> /etc/rc.local
 
 #Inicializa o programa.
@@ -24,4 +24,4 @@ echo "screen -dmS bot_teste ./botteste" >> /etc/rc.local
 screen -dmS bot_teste ./botteste
 
 # Remove instalador
-rm /etc/SSHPlus/install_bot.sh
+rm /usr/local/bin/install_bot.sh
